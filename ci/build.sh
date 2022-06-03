af6d800082c9b78b1ead0d0fd9c135d94c7a3629
Ilias <ilias.kelesoglu@globule.net>
2022-06-03 23:57:35 +0200

    Modify Unity Path

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,7 @@ echo "Building for $BUILD_TARGET"
 export BUILD_PATH=$UNITY_DIR/Builds/$BUILD_TARGET/
 mkdir -p $BUILD_PATH
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' unity-editor} \
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
   -projectPath $UNITY_DIR \
   -quit \
   -batchmode \
